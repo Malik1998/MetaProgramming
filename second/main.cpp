f98c0e10740aba7045b781668506d1db8ebaf890
Malik Gazizullin <gazizullin.malik2010@yandex.ru>
2019-10-17 22:26:24 +0300

    Second HW

---
+++ second/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "GetIndexOfPrivate.h"
+#include <string>
+
+struct SmallType {
+
+};
+
+struct MyType {
+
+    int a, b, c;
+    std::string d;
+    SmallType small;
+};
+
+int main() {
+
+    std::cout << GetIndexOf< Tuple<int,Tuple<double, Tuple<float, NullType>>>, float>()() << " == " << 2 << " ? \n";
+    std::cout << GetIndexOf< Tuple<int,Tuple<MyType, Tuple<float, NullType>>>, MyType>()() << " == " << 1 << " ? \n";
+    std::cout << GetIndexOf< Tuple<int,Tuple<double, Tuple<float, NullType>>>, std::string>()() << " == " << -1 << " ? \n";
+    std::cout << GetIndexOf< Tuple<int,Tuple<double, Tuple<SmallType, NullType>>>, SmallType>()() << " == " << 2 << " ? \n";
+    std::cout << GetIndexOf< Tuple<int,Tuple<double, Tuple<float, NullType>>>, MyType>()() << " == " << -1 << " ? \n";
+}
